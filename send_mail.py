425bc2ea3fc68c2a6a4da01cfd6673f591d33deb
Amit Robert Baroi <amitrobertbaroi@gmail.com>
2019-12-08 20:09:50 +0600

    Added script to implement email functionality

---
+++ send_mail.py
@@ -0,0 +1,22 @@
+import smtplib
+from email.mime.text import MIMEText
+
+
+def send_mail(customer, dealer, rating, comments):
+    host = 'smtp.mailtrap.io'
+    port = 2525
+    user_name = 'c0ece7179608e4'
+    password = '7ce303bd5a834b'
+
+    message = f'<h3>New Feedback Submission</h3><ul><li>Customer: {customer}</li><li>Dealer: {dealer}</li><li>Rating: {rating}</li><li>Comments: {comments}</li></ul>'
+    sender_email = 'website@example.com'
+    reciever_email = 'reciever@example.com'
+    msg = MIMEText(message, 'html')
+    msg['Subject'] = 'Feedback received'
+    msg['From'] = sender_email
+    msg['To'] = reciever_email
+
+    # Send email
+    with smtplib.SMTP(host, port) as server:
+        server.login(user_name, password)
+        server.sendmail(sender_email, reciever_email, msg.as_string())
