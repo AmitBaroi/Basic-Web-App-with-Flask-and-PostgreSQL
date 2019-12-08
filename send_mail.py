651f5d5a5f4d41b1c11d2bd59827df62df6bcf5b
Amit Robert Baroi <amitrobertbaroi@gmail.com>
2019-12-08 20:33:22 +0600

    Minor modifications: variable names, code readability

--- send_mail.py
+++ send_mail.py
@@ -3,20 +3,21 @@ from email.mime.text import MIMEText
 
 
 def send_mail(customer, dealer, rating, comments):
+    sender_email = 'webapp@example.com'
+    receiver_email = 'receiver@example.com'
+
     host = 'smtp.mailtrap.io'
     port = 2525
     user_name = 'c0ece7179608e4'
     password = '7ce303bd5a834b'
-
     message = f'<h3>New Feedback Submission</h3><ul><li>Customer: {customer}</li><li>Dealer: {dealer}</li><li>Rating: {rating}</li><li>Comments: {comments}</li></ul>'
-    sender_email = 'website@example.com'
-    receiver_email = 'receiver@example.com'
+
     msg = MIMEText(message, 'html')
     msg['Subject'] = 'Feedback received'
     msg['From'] = sender_email
     msg['To'] = receiver_email
 
-    # Send email
+    # Sending email
     with smtplib.SMTP(host, port) as server:
         server.login(user_name, password)
         server.sendmail(sender_email, receiver_email, msg.as_string())
